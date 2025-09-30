3a69f568d0ba3e173dc5a8943f587e5dd44428f8
PatrickAOliveira <patrickoli99@gmail.com>
2025-09-30 17:55:11 -0300

    criando últimas seções do projeto, ajustando responsividade mobile

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
-Preview do Projeto
+Preview do Projeto, criado com a finalidade de estudo durante meu curso, durante sua construção foram utilizados as seguintes ferramentas: NodeJS, SASS, Gulp, HTML5, CSS e JavaScript. O site não possui back-end funcional, apenas front-end.
+
 <img width="1354" height="645" alt="heroDisneyPlus" src="https://github.com/user-attachments/assets/5d83cd6a-45cd-4118-baf3-80e6c187056e" />
 <img width="1353" height="645" alt="landpageDisneyPlus" src="https://github.com/user-attachments/assets/5cab43cc-a842-4dcf-a929-d0648656cafd" />
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const buttons = document.querySelectorAll("[data-tab-button]");
+	const questions = document.querySelectorAll("[data-faq-question]");
 
+	const heroSection = document.querySelector(".hero");
+	const alturaHero = heroSection.clientHeight;
+
+	window.addEventListener("scroll", function () {
+		const posicaoAtual = window.scrollY;
+
+		if (posicaoAtual < alturaHero) {
+			hiddenHeaderElem();
+		} else {
+			showHeaderElem();
+		}
+	});
+
+	// Seção de atrações, programação das abas / tabs
 	for (let i = 0; i < buttons.length; i++) {
 		buttons[i].addEventListener("click", function (botao) {
 			const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +26,29 @@ document.addEventListener("DOMContentLoaded", function () {
 			botao.target.classList.add("shows__tabs__button--active");
 		});
 	}
+
+	// Seção FAQ, accordion
+	for (let i = 0; i < questions.length; i++) {
+		questions[i].addEventListener("click", abreOuFechaResposta);
+	}
 });
+
+function hiddenHeaderElem() {
+	const header = document.querySelector("header");
+	header.classList.add("header--is-hidden");
+}
+
+function showHeaderElem() {
+	const header = document.querySelector("header");
+	header.classList.remove("header--is-hidden");
+}
+
+function abreOuFechaResposta(e) {
+	const classe = "faq__questions__item--is-open";
+	const elementoPai = e.target.parentNode;
+
+	elementoPai.classList.toggle(classe);
+}
 
 function removeActiveButton() {
 	const buttons = document.querySelectorAll("[data-tab-button]");
